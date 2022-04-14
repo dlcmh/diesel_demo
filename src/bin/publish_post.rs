72e0df03743cceb7fd3d01cc1c61dad4cd6ec529
David Chin <dlcmhd@me.com>
2022-04-14 17:10:08 +0800

    08 - Rust code to update a post

---
+++ src/bin/publish_post.rs
@@ -0,0 +1,25 @@
+extern crate diesel;
+extern crate diesel_demo;
+
+use diesel::prelude::*;
+use diesel_demo::*;
+use models::Post;
+use std::env::args;
+
+fn main() {
+    use diesel_demo::schema::posts::dsl::{posts, published};
+
+    let id = args()
+        .nth(1)
+        .expect("publish_post requires a post id")
+        .parse::<i32>()
+        .expect("Invalid ID");
+    let connection = establish_connection();
+
+    let post = diesel::update(posts.find(id))
+        .set(published.eq(true))
+        .get_result::<Post>(&connection)
+        .expect(&format!("Unable to find post {}", id));
+
+    println!("Published post {}", post.title);
+}
